e0805bfa8d3b545bd72c8d6f9935727f8a598b8a
Marco Barroso <marcobneves@gmail.com>
2016-01-28 15:09:26 -0400

    Projeto realTime

---
+++ microtwitter.js
@@ -0,0 +1,24 @@
+Mensagem = new Meteor.Collection('mensagem');
+if (Meteor.isClient) {
+  Template.formulario.events({
+    'click #enviar': function(event, template) {
+      var nome = template.find('#nome').value;
+      var conteudo = template.find('#conteudo').value;
+      Mensagem.insert({ nome: nome,
+                        conteudo: conteudo,
+                        data: new Date().toLocaleString() });
+      event.preventDefault();
+    }
+  });
+
+  Template.mensagens.timeline = function() {
+    return Mensagem.find();
+  };
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    Mensagem.remove({});
+    console.log("Rodando MicroTwitter");
+  });
+}
